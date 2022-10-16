d14cc0e28fa06f260634fc068d70e0e75febdb27
FelipeKreulich <felipek2002k@gmail.com>
2022-10-16 16:57:43 -0400

    Starter Commit

---
+++ src/data/testimonialsData.js
@@ -0,0 +1,25 @@
+import image1 from "../assets/t-image1.png";
+import image2 from "../assets/t-image2.jpg";
+import image3 from "../assets/t-image3.jpg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review:
+      "Fiz a escolha certa ao escolher o Fitclub e ao escolher o plano e programa certo já alcancei o meu corpo ideal!",
+    name: 'ALAN DA SILVA',
+    status : 'EMPREENDEDOR'
+  },
+  {
+    image: image2,
+    review: 'Sinto que minha vida melhorou ao escolher o Fitclub, aulas e Coachs excepcionais!',
+    name: 'KELVEN SANTOS',
+    status: 'VENDEDOR'
+  },
+  {
+    image : image3,
+    review:'Acabou o meu stresse com meu próprio corpo, a Fitclub me motiva todos os dias a ser uma pessoa melhor!',
+    name: 'CAIO CRUZ',
+    status: "MECÂNICO"
+  }
+];
