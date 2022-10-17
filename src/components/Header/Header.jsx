232c5caea7138d2714736383499c7795d6174ab7
FelipeKreulich <felipek2002k@gmail.com>
2022-10-16 20:46:21 -0400

    Create a Hero Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,14 @@ import Logo from '../../assets/logo.png'
 const Header = () => {
   return (
     <div className="header">
-      <img src={Logo} alt="Logo" />
+      <img src={Logo} alt="Logo" className='logo' />
+      <ul className='header-menu'>
+        <li>Início</li>
+        <li>Programas</li>
+        <li>Sobre Nós</li>
+        <li>Planos</li>
+        <li>Comentários</li>
+      </ul>
     </div>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,76 @@
 import React from 'react'
 import './Hero.css'
 import Header from '../Header/Header'
+import hero_image from '../../assets/hero_image.png'
+import hero_image_back from '../../assets/hero_image_back.png'
+import Heart from '../../assets/heart.png'
+import Calories from '../../assets/calories.png'
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="left-h">
         <Header />
+        {/* THE BEST AD */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>O melhor club fitness da cidade</span>
+        </div>
+        {/* HERO HEADING */}
+        <div className="hero-text">
+          <div>
+            <span className='stroke-text'>Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal Body</span>
+          </div>
+          <div>
+            <span>
+              Aqui vamos ajudá-lo a moldar e construir seu corpo ideal e viver sua vida ao máximo.
+            </span>
+          </div>
+        </div>
+        {/* FIGURES */}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>Excelentes Coachs</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>Membros no Clube</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>Programas Fitness</span>
+          </div>
+        </div>
+        {/* HERO BUTTONS */}
+        <div className="hero-buttons">
+          <button className="btn">Vamos lá</button>
+          <button className="btn">Leia Mais</button>
+        </div>
       </div>
-      <div className="right-h"></div>
+      <div className="right-h">
+        <button className='btn'>Entre já</button>
+        <div className="heart-rate">
+          <img src={Heart} alt="Coração" />
+          <span>Heart Rate</span>
+          <span>116 BPM</span>
+        </div>
+        {/* HERO IMAGES */}
+        <img src={hero_image} alt="Hero" className="hero-image" />
+        <img src={hero_image_back} alt="Hero" className="hero-image-back" />
+        {/* CALORIES */}
+        <div className="calories">
+          <img src={Calories} alt="Calorias" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
